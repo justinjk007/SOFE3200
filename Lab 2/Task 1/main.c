dc81e61ecb0388f16d4b40e5247a072348e132e0
Justin K -School <justinjoseph0007@gmail.com>
2017-10-14 21:24:58 -0400

    Clang formatting

--- Lab 2/Task 1/main.c
+++ Lab 2/Task 1/main.c
@@ -1,53 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main()
 {
     FILE *fptr, *fpt, *file;
- 
-    char a, b, c, d, e;
- 
-    // Open file
-    fptr = fopen("Input.txt", "r"); 
 
-    printf ("The contents of Input.txt is: \n\n");
- 
+    char a, b, c, d, e;
+
+    // Open file
+    fptr = fopen("Input.txt", "r");
+
+    printf("The contents of Input.txt is: \n\n");
+
     // Read contents from file
     c = fgetc(fptr);
-    while (c != EOF)
-    {
-        printf ("%c", c);
-  	c = fgetc(fptr);
+    while (c != EOF) {
+        printf("%c", c);
+        c = fgetc(fptr);
     }
 
     a = 'a';
     b = 'b';
 
-    fpt = fopen("Input.txt", "r+");
+    fpt  = fopen("Input.txt", "r+");
     file = fopen("Heaven.txt", "w");
 
-    printf ("\nThe contents of Input.txt after changing a's with b's is: \n\n");
+    printf("\nThe contents of Input.txt after changing a's with b's is: \n\n");
 
     d = fgetc(fpt);
     e = fgetc(file);
 
-    while (d  != EOF)
-    {
+    while (d != EOF) {
         if (d == a) {
             d = b;
         }
-    
-    printf ("%c", d);
-    fprintf (file, "%c", d);
-   
-    e = fgetc(file);
-    d = fgetc(fpt);
+
+        printf("%c", d);
+        fprintf(file, "%c", d);
+
+        e = fgetc(file);
+        d = fgetc(fpt);
     }
 
-    fclose(file); 
+    fclose(file);
     fclose(fptr);
     fclose(fpt);
 
     return 0;
 }
-
