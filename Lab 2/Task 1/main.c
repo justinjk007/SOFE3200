b2297616b591968f02554df1933902d01eaf73c0
Justin K -School <justinjoseph0007@gmail.com>
2017-10-14 21:58:18 -0400

    Fully reviewed, 10/10 Zach

--- Lab 2/Task 1/main.c
+++ Lab 2/Task 1/main.c
@@ -3,41 +3,39 @@
 
 int main()
 {
-    FILE *fptr, *fpt, *out_file;
-    char a, b, c, d, e;
+    FILE *fptr, *out_file;
+    char a, b, c, d;
 
-    // Open file
+    // Open file to read contents
     fptr = fopen("Input.txt", "r");
-    printf("The contents of Input.txt is: \n\n");
-    // Read contents from file
     c = fgetc(fptr);
     // Display contents of the file
+    printf("The contents of Input.txt is: \n\n");
     while (c != EOF) {
         printf("%c", c);
         c = fgetc(fptr);
     }
+    rewind(fptr); // Reset pointer to the beginning of the file
 
     a        = 'a';  // I don't know what to say here
     b        = 'b';
-    fpt      = fopen("Input.txt", "r+");  // Open file for reading and more
     out_file = fopen("Heaven.txt", "w");  // Open file for writing
     printf("\nThe contents of Input.txt after changing a's with b's is: \n\n");
-    d = fgetc(fpt);
-    e = fgetc(out_file);
+    d = fgetc(fptr);
 
+    // Display contents of the file after editing
     while (d != EOF) {
         if (d == a) {
             d = b;
         }
-        printf("%c", d);
-        fprintf(out_file, "%c", d);
-        e = fgetc(out_file);
-        d = fgetc(fpt);
+        printf("%c", d); // Print file after editing
+        fprintf(out_file, "%c", d); // Save edited contents into new file
+        d = fgetc(fptr);
     }
-    // Close every file opened
+
+    // Close both opened files
     fclose(out_file);
     fclose(fptr);
-    fclose(fpt);
-
     return 0;  // We are safe
+
 }
