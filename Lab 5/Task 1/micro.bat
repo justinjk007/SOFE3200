c06b9db635b2043617f60f4e8bf3cbf471dbc464
GZakharov1525 <GZakharov1525@users.noreply.github.com>
2017-12-01 11:19:11 -0500

    task 1 files upload

---
+++ Lab 5/Task 1/micro.bat
@@ -0,0 +1,8 @@
+@echo off
+title Lab 5 task 1
+echo This batch script will open Microsoft Visual Studio 2017 and DOS cmd.
+pause
+echo OpeningMSVC2017
+START C:\"Program Files (x86)"\"Microsoft Visual Studio"\2017\Professional\Common7\IDE\devenv.exe
+echo Opening cmd 
+START cmd /k echo This window was opened with a batch script.
